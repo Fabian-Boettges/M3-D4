4893028a76dd6fbe3a568bc54674825433683e93
Fabian-Boettges <95299029+Fabian-Boettges@users.noreply.github.com>
2022-03-03 17:20:50 +0100

    fetch + display

---
+++ index.js
@@ -0,0 +1,27 @@
+// 0) Get all the products from the API using a fetch
+
+const fetchProducts = fetch("https://striveschool-api.herokuapp.com/books")
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data)
+
+        const row = document.querySelector("#mainContainer")
+        data.forEach((element) => {
+            const content = ` 
+            <div class="col-3">
+                <div class="card">
+                    <img src="${element.img}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${element.title}</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            
+            </div>
+            `
+
+            row.innerHTML += content
+
+        })
+    })
