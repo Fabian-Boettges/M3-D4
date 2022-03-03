b724f73970796ee5f3868b4cc062351219512a33
Fabian-Boettges <95299029+Fabian-Boettges@users.noreply.github.com>
2022-03-03 17:27:12 +0100

    added some margins

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const fetchProducts = fetch("https://striveschool-api.herokuapp.com/books")
         const row = document.querySelector("#mainContainer")
         data.forEach((element) => {
             const content = ` 
-            <div class="col-3">
+            <div class="col-3 mt-3 mr-0">
                 <div class="card">
-                    <img src="${element.img}" class="card-img-top" alt="...">
+                    <img src="${element.img}" class="card-img-top" style="height:100%; cover" "alt="...">
                     <div class="card-body">
                         <h5 class="card-title">${element.title}</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
